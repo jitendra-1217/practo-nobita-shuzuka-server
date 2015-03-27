48d4078bc013abb525bfe8858930873f12c97f4a
Jitendra K <jitendraojha@outlook.com>
2015-03-28 00:21:38 +0530

    update token added.

--- doctor_api/app.py
+++ doctor_api/app.py
@@ -7,6 +7,8 @@ app = Flask(__name__)
 mysql = MySQL()
 mysql.init_app(app)
 app.config.from_pyfile('../flaskapp.cfg')
+conn = mysql.connect()
+cursor = conn.cursor()
 
 @doctor_api_bp.route('/')
 def index():
@@ -18,8 +20,6 @@ def testConfig():
 
 @doctor_api_bp.route('/put-tokens', methods=['POST'])
 def putTokens():
-    con = mysql.connect()
-    cursor = con.cursor()
     cursor.execute('select avg_checkup_time from doctors where id in (select doctor_id from doctor_locations where id = "%d")' % (int(request.form['doctor_location_id'])))
     data = cursor.fetchone()
     avgCheckupTimeInMin = str(data[0])
@@ -27,5 +27,11 @@ def putTokens():
     for serialNo in range(1, int(request.form['no_of_tokens']) + 1):
         cursor.execute('insert into tokens (serial_no, token_timestamp, start_time, status, doctor_location_id) values ("%d", "%s", "%s", "empty", "%d")' % (serialNo, request.form['token_timestamp'], startTime, int(request.form['doctor_location_id'])))
         startTime = (datetime.datetime.strptime(startTime, "%H:%M:%S") + datetime.timedelta(minutes = int(avgCheckupTimeInMin))).strftime("%H:%M:%S")
-    con.commit()
+    conn.commit()
+    return jsonify(message='success')
+
+@doctor_api_bp.route('/update-token', methods=['POST'])
+def updateToken():
+    cursor.execute('update tokens set status = "%s" where id = "%d"' % (request.form['status'], int(request.form['id'])))
+    conn.commit()
     return jsonify(message='success')
