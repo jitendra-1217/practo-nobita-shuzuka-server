c63cedb5b5dad8abc8327c9bce72e39243438dff
Jitendra K <jitendraojha@outlook.com>
2015-03-27 22:30:54 +0530

    Testing one api with mysql operation.

--- doctor_api/app.py
+++ doctor_api/app.py
@@ -1,8 +1,11 @@
-from flask import Blueprint, jsonify, Flask
+from flask import Blueprint, jsonify, Flask, request
+from flaskext.mysql import MySQL
 doctor_api_bp = Blueprint('doctor_api',__name__,template_folder='templates')
 
 app = Flask(__name__)
 app.config.from_envvar('NOALPHA_SETTINGS')
+mysql = MySQL()
+mysql.init_app(app)
 
 @doctor_api_bp.route('/')
 def index():
@@ -11,3 +14,11 @@ def index():
 @doctor_api_bp.route('/test-config')
 def testConfig():
     return app.config['APP_NAME']
+
+@doctor_api_bp.route('/put-tokens', methods=['POST'])
+def putTokens():
+    con = mysql.connect()
+    cursor = con.cursor()
+    cursor.execute('insert into tokens (serial_no, start_time, status, doctor_location_id) values (1, \'' + request.form['start_time'] + '\', \'empty\', ' + request.form['doctor_location_id'] + ')')
+    con.commit()
+    return jsonify(message='success')
