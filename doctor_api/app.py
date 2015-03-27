51d2b4ca9a50ea053532071d63c426dcddad1b3a
Manoj Hans <hash_practo@Manojs-MacBook-Pro.local>
2015-03-27 20:42:39 +0530

    config changed

--- doctor_api/app.py
+++ doctor_api/app.py
@@ -2,7 +2,7 @@ from flask import Blueprint, jsonify, Flask
 doctor_api_bp = Blueprint('doctor_api',__name__,template_folder='templates')
 
 app = Flask(__name__)
-app.config.from_envvar('NOALPHA_SETTINGS')
+app.config.from_pyfile('flaskapp.cfg')
 
 @doctor_api_bp.route('/')
 def index():
