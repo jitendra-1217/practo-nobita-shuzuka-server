8af375cf1ecd4cb238f481e90108bad90db8bca8
Jitendra K <jitendraojha@outlook.com>
2015-03-27 23:39:28 +0530

    put tokens api.

--- doctor_api/app.py
+++ doctor_api/app.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, Flask, request
 from flaskext.mysql import MySQL
+import datetime
 doctor_api_bp = Blueprint('doctor_api',__name__,template_folder='templates')
 
 app = Flask(__name__)
@@ -19,6 +20,12 @@ def testConfig():
 def putTokens():
     con = mysql.connect()
     cursor = con.cursor()
-    cursor.execute('insert into tokens (serial_no, start_time, status, doctor_location_id) values (1, \'' + request.form['start_time'] + '\', \'empty\', ' + request.form['doctor_location_id'] + ')')
+    cursor.execute('select avg_checkup_time from doctors where id in (select doctor_id from doctor_locations where id = "%d")' % (int(request.form['doctor_location_id'])))
+    data = cursor.fetchone()
+    avgCheckupTimeInMin = str(data[0])
+    startTime = request.form['start_time']
+    for serialNo in range(1, int(request.form['no_of_tokens']) + 1):
+        cursor.execute('insert into tokens (serial_no, start_time, status, doctor_location_id) values ("%d", "%s", "empty", "%d")' % (serialNo, startTime, int(request.form['doctor_location_id'])))
+        startTime = (datetime.datetime.strptime(startTime, "%H:%M:%S") + datetime.timedelta(minutes = int(avgCheckupTimeInMin))).strftime("%H:%M:%S")
     con.commit()
     return jsonify(message='success')
