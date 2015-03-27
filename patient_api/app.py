a040dc989053d6260808642a8b96164ee4f788b9
Manoj Hans <hash_practo@Manojs-MacBook-Pro.local>
2015-03-27 21:45:20 +0530

    flaskconfig added to apps

--- patient_api/app.py
+++ patient_api/app.py
@@ -2,7 +2,7 @@ from flask import Blueprint, jsonify, Flask
 patient_api_bp = Blueprint('patient_api',__name__,template_folder='templates')
 
 app = Flask(__name__)
-app.config.from_pyfile('flaskapp.cfg')
+app.config.from_pyfile('../flaskapp.cfg')
 
 @patient_api_bp.route('/')
 def index():
